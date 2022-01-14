5806ddf01eec215e7db0bad4ad40c2cd45277692
David Forsyth Rosin <drosin96@gmail.com>
2022-01-14 15:59:32 +0100

    Added a dice element as a test and proof of concept

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const app = Vue.createApp({});
 
+app.component("dice", {
+  data() {
+    return { rollDice: Math.round(Math.random() * 5) };
+  },
+  methods: {
+    onClick() {
+      this.rollDice = Math.round(Math.random() * 5);
+    },
+  },
+  template: `<input @click="onClick" type="button" :value="rollDice + 1">`,
+});
+
 app.mount("#app");
